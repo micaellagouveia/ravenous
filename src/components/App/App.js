4beeb3285d674cc53e624eee9d32341389a8a540
Micaella Gouveia <micaella2212@gmail.com>
2020-05-19 13:43:38 -0300

    Modify css and list business with props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,26 @@ import './App.css';
 import BusinessList from '../BusinessList/BusinessList.js'
 import SearchBar from '../SearchBar/SearchBar.js'
 
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+const businesses = [business, business, business, business, business, business]
+
 export default class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>Ravenous</h1>
-        <BusinessList />
+        <BusinessList businesses={businesses} />
         <SearchBar />
       </div>
     );
